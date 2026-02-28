7cacd9e779959dd783ece806b34cf50cdaaf1337
xiaokang <2395711943@qq.com>
2026-03-01 01:25:02 +0800

    修复 arm64 的 NDK 构建入口

--- android/ijkplayer/ijkplayer-arm64/src/main/jni/Android.mk
+++ android/ijkplayer/ijkplayer-arm64/src/main/jni/Android.mk
@@ -1,2 +1,11 @@
 LOCAL_PATH := $(call my-dir)
+
+MY_APP_JNI_ROOT := $(realpath $(LOCAL_PATH))
+MY_APP_PRJ_ROOT := $(realpath $(MY_APP_JNI_ROOT)/..)
+MY_APP_ANDROID_ROOT := $(realpath $(MY_APP_PRJ_ROOT)/../../../..)
+
+MY_APP_FFMPEG_OUTPUT_PATH := $(realpath $(MY_APP_ANDROID_ROOT)/contrib/build/ffmpeg-arm64/output)
+MY_APP_FFMPEG_INCLUDE_PATH := $(realpath $(MY_APP_FFMPEG_OUTPUT_PATH)/include)
+MY_APP_FFMPEG_SOURCE_PATH := $(MY_APP_FFMPEG_INCLUDE_PATH)
+
 include $(call all-subdir-makefiles)
